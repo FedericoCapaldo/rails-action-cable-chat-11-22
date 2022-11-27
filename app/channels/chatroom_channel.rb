6b6470df8c380965bc666b0f57f1e35f449e2e99
Federico <federico.capaldo@hotmail.com>
2022-11-27 21:19:51 +0000

    subscribing to different channels based on id instead of hardcoded general channel

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,7 +1,8 @@
 class ChatroomChannel < ApplicationCable::Channel
   # run when joining chat
   def subscribed
-    stream_from "general"
+    chatroom = Chatroom.find(params[:id])
+    stream_for chatroom
   end
 
   # run when leaving chat
--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,9 +4,14 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
+  static values = { chatroomId: Number }
   static targets = ["messages"]
 
   connect() {
-    createConsumer().subscriptions.create('ChatroomChannel');
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+    createConsumer().subscriptions.create({
+      channel: 'ChatroomChannel',
+      id: this.chatroomIdValue
+    });
   }
 }
